d698b356aa85104ae9102034bf7380af5b05be0b
Bernardo Fontes <bernardoxhc@gmail.com>
2025-04-13 21:18:14 +0200

    Configuração mínima para conseguir renderizar a home

--- posts/urls.py
+++ posts/urls.py
@@ -1,15 +1,15 @@
 from django.urls import path
 
-from posts.views import HomeIndexView
+from posts.views import *
 
 app_name = 'poems'
 
 urlpatterns = [
     path('', HomeIndexView.as_view(), name='index'),
-    # re_path(r'^todos/$', list_all, name='list_all'),
-    # re_path(r'^aleatorio/$', random_post_view, name='random'),
-    # re_path(r'^autor/(?P<author_username>[\w\d-]+)/$', list_by_author, name='list_by_author'),
-    # re_path(r'^texto/(?P<post_slug>[\w\d-]+)/$', post_detail, name='post_detail'),
-    # re_path(r'^categoria/(?P<category_slug>[\w\d-]+)/$', category_start, name='category'),
-    # re_path(r'^categoria/(?P<category_slug>[\w\d-]+)/(?P<post_slug>[\w\d-]+)/$', post_category_detail, name='cat_post_detail'),
+    path('todos/', ListAllView.as_view(), name='list_all'),
+    path('aleatorio/', random_post_view, name='random'),
+    path('autor/<slug:author_username>/', ListByAuthorView.as_view(), name='list_by_author'),
+    path('texto/<slug:post_slug>/', PostDetailView.as_view(), name='post_detail'),
+    # path('categoria/(?P<category_slug>[\w\d-]+)/$', category_start, name='category'),
+    # path('categoria/(?P<category_slug>[\w\d-]+)/(?P<post_slug>[\w\d-]+)/$', post_category_detail, name='cat_post_detail'),
 ]
