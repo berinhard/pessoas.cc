acdc1e339edef6c3a8e4791aa407a5daee02f426
Bernardo Fontes <bernardoxhc@gmail.com>
2025-04-13 21:24:47 +0200

    Já prepara o static root para ser lido a partir de env e deixar mais fácil

--- pessoas/settings.py
+++ pessoas/settings.py
@@ -95,7 +95,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = config("STATIC_ROOT", cast=Path, default=BASE_DIR / 'staticfiles')
 
 
 # Default primary key field type
