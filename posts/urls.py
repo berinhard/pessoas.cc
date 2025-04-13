369ca10a100c8ca0bb456c93ec860a258effb623
Bernardo Fontes <bernardoxhc@gmail.com>
2025-04-13 21:31:33 +0200

    Ativa todas as URLs

--- posts/urls.py
+++ posts/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
     path('aleatorio/', random_post_view, name='random'),
     path('autor/<slug:author_username>/', ListByAuthorView.as_view(), name='list_by_author'),
     path('texto/<slug:post_slug>/', PostDetailView.as_view(), name='post_detail'),
-    # path('categoria/(?P<category_slug>[\w\d-]+)/$', category_start, name='category'),
-    # path('categoria/(?P<category_slug>[\w\d-]+)/(?P<post_slug>[\w\d-]+)/$', post_category_detail, name='cat_post_detail'),
+    path('categoria/<slug:category_slug>/', category_start, name='category'),
+    path('categoria/<slug:category_slug>/<slug:post_slug>/', post_category_detail, name='cat_post_detail'),
 ]
